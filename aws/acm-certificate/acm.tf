7d914fd8c891f7b98f26c479ceae873422e67994
Dieb Sami Maher (DFDS A/S) <samdi@DFDS.COM>
2019-10-23 16:31:18 +0200

    what!

--- aws/acm-certificate/acm.tf
+++ aws/acm-certificate/acm.tf
@@ -1,5 +1,19 @@
+# locals {
+#   domain_names = "${concat([var.domain_name], var.subject_alternative_names)}"
+# }
+
 locals {
-  domain_names = "${concat([var.domain_name], var.subject_alternative_names)}"
+  // Get distinct list of domains and SANs
+  distinct_domain_names = distinct(concat([var.domain_name], data.template_file.breakup_san.*.rendered))
+
+  // Copy domain_validation_options for the distinct domain names
+  validation_domains = [for k, v in aws_acm_certificate.this[0].domain_validation_options : tomap(v) if contains(local.distinct_domain_names, v.domain_name)]
+}
+
+data "template_file" "breakup_san" {
+  count = length(var.subject_alternative_names)
+
+  template = replace(var.subject_alternative_names[count.index], "*.", "")
 }
 
 # Create the certificate request
@@ -14,14 +28,30 @@ resource "aws_acm_certificate" "cert" {
 }
 
 # Create validation DNS record(s) in the specified DNS zone (alternative names specified)
+# resource "aws_route53_record" "validation" {
+#   count = "${var.validation_method == "DNS" ? length(local.domain_names) : 0}"
+#   name = "${lookup(aws_acm_certificate.cert.domain_validation_options[count.index], "resource_record_name")}"
+#   type = "${lookup(aws_acm_certificate.cert.domain_validation_options[count.index], "resource_record_type")}"
+#   zone_id ="${var.dns_zone_id}"
+#   records = ["${lookup(aws_acm_certificate.cert.domain_validation_options[count.index], "resource_record_value")}"]
+#   ttl = 60
+# }
 resource "aws_route53_record" "validation" {
-  count = "${var.validation_method == "DNS" ? length(local.domain_names) : 0}"
-  name = "${lookup(aws_acm_certificate.cert.domain_validation_options[count.index], "resource_record_name")}"
-  type = "${lookup(aws_acm_certificate.cert.domain_validation_options[count.index], "resource_record_type")}"
-  zone_id ="${var.dns_zone_id}"
-  records = ["${lookup(aws_acm_certificate.cert.domain_validation_options[count.index], "resource_record_value")}"]
-  ttl = 60
+  count = var.validation_method == "DNS" ? length(local.distinct_domain_names) : 0
+
+  zone_id = var.zone_id
+  name    = local.validation_domains[count.index]["resource_record_name"]
+  type    = local.validation_domains[count.index]["resource_record_type"]
+  ttl     = 60
+
+  records = [
+    local.validation_domains[count.index]["resource_record_value"]
+  ]
+
+  # allow_overwrite = var.validation_allow_overwrite_records
 }
+
+
 
 # Validate the certificate using the DNS validation records created
 # This resource represents a successful validation of an ACM certificate in concert with other resources.
