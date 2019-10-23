29a29051e9154775bb72f46699e3c7413b59a282
Bertil Ljungbeck <belju@DFDS.COM>
2019-10-23 15:26:31 +0200

    Removed backend declaration in terraform example

--- aws/s3-bucket/example/terraform/main.tf
+++ aws/s3-bucket/example/terraform/main.tf
@@ -1,9 +1,8 @@
 terraform {
   required_version = "~> 0.12.2"
-  backend "s3" {}
 }
 
-// Region an dother vars are using the default values defined by the module: using DFDS default value defined in module ()"eu-central-1")
+// Region and other vars are using the default values defined by the module: using DFDS default value defined in module ()"eu-central-1")
 
 module "create_s3bucket_using_module" {
   //source = "git::https://github.com/dfds-frontend/terraform-modules.git//aws/s3-bucket" // To be used with tagging as well, this line is to serve as example for how this should be referenced in real world scenarios. The example here always refers to current local copy, hence the source definition below.
