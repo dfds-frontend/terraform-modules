9145239f5f32ef2ac9e415ebf69429b475297db8
Dieb Sami Maher (DFDS A/S) <samdi@DFDS.COM>
2019-10-23 12:42:54 +0200

    test disabling validation

--- aws/acm-certificate/acm.tf
+++ aws/acm-certificate/acm.tf
@@ -2,20 +2,19 @@ locals {
   domain_names = "${concat([var.domain_name], var.subject_alternative_names)}"
 }
 
-# Create the certificate request
+
 resource "aws_acm_certificate" "main" {
   domain_name = "${local.domain_names[0]}"
   subject_alternative_names = "${slice(local.domain_names, 1, length(local.domain_names))}"
-  validation_method = "DNS"
+  validation_method = "NONE"
   lifecycle {
     create_before_destroy = true
     ignore_changes = ["subject_alternative_names"] # workaround to https://github.com/terraform-providers/terraform-provider-aws/issues/8531
   }
 }
 
-# Create validation DNS record(s) in the specified DNS zone (alternative names specified)
 resource "aws_route53_record" "validation" {
-  count   = "${var.create_validation ? length(ocal.domain_names) : 0}"
+  count = "${length(local.domain_names)}"
   name = "${lookup(aws_acm_certificate.main.domain_validation_options[count.index], "resource_record_name")}"
   type = "${lookup(aws_acm_certificate.main.domain_validation_options[count.index], "resource_record_type")}"
   zone_id ="${var.dns_zone_id}"
@@ -23,9 +22,7 @@ resource "aws_route53_record" "validation" {
   ttl = 60
 }
 
-# Validate the certificate using the DNS validation records created
 resource "aws_acm_certificate_validation" "main" {
-  count   = "${var.create_validation ? 1 : 0}"
   certificate_arn = "${aws_acm_certificate.main.arn}"
   validation_record_fqdns = "${aws_route53_record.validation.*.fqdn}"
 }
