a0c88f8a36cf081618ce6df098abf7d85edc2728
Dieb Sami Maher (DFDS A/S) <samdi@DFDS.COM>
2019-10-23 13:47:20 +0200

    asd

--- aws/acm-certificate/acm.tf
+++ aws/acm-certificate/acm.tf
@@ -6,7 +6,7 @@ locals {
 resource "aws_acm_certificate" "main" {
   domain_name = "${local.domain_names[0]}"
   subject_alternative_names = "${slice(local.domain_names, 1, length(local.domain_names))}"
-  validation_method = "${var.validation_method}"
+  validation_method = null # "${var.validation_method}"
   lifecycle {
     create_before_destroy = true
     ignore_changes = ["subject_alternative_names"] # workaround to https://github.com/terraform-providers/terraform-provider-aws/issues/8531
