09ea0b2f427067e5c2fbbdaf91693342d50fcb8b
Dieb Sami Maher (DFDS A/S) <samdi@DFDS.COM>
2019-10-23 16:15:37 +0200

    sdfdsf

--- aws/acm-certificate/acm.tf
+++ aws/acm-certificate/acm.tf
@@ -26,7 +26,7 @@ resource "aws_route53_record" "validation" {
 # Validate the certificate using the DNS validation records created
 # This resource represents a successful validation of an ACM certificate in concert with other resources.
 # WARNING: This resource implements a part of the validation workflow. It does not represent a real-world entity in AWS, therefore changing or deleting this resource on its own has no immediate effect.
-resource "aws_acm_certificate_validation" "main" {
+resource "aws_acm_certificate_validation" "cert" {
   count = "${var.validation_method == "DNS" && var.wait_for_validation ? 1 : 0 }"
   certificate_arn = "${aws_acm_certificate.cert.arn}"
   validation_record_fqdns = "${aws_route53_record.validation.*.fqdn}"
