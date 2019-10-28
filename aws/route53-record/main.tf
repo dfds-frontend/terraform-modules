cc923135972e5143cadcc2c062b47a1b33bb9fb1
Dieb Sami Maher (DFDS A/S) <samdi@DFDS.COM>
2019-10-28 10:15:09 +0100

    record module

--- aws/route53-record/main.tf
+++ aws/route53-record/main.tf
@@ -2,8 +2,8 @@ resource "aws_route53_record" "record" {
   zone_id = "${var.zone_id}"
   name    = "${var.record_name}"
   type    = "${var.record_type}"
-  records = "${var.record_type == "CNAME" ? [var.record_value] : null}"
-  ttl     = "${var.record_type == "CNAME" ? [var.record_ttl] : null}"
+  records = "${var.record_value ? [var.record_value] : null}"
+  ttl     = "${var.record_ttl}"
 
   dynamic "alias" {
     for_each = "${var.record_type == "A" ? [1] : [] }"
