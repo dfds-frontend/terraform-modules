59578ec15b2442e8ede8aaff0835e32cff791505
Bertil Ljungbeck <belju@DFDS.COM>
2019-10-29 16:47:36 +0100

    Corrected source on example for certificate.tf

--- aws/example/cloudfront_custom_certificate_logging_s3bucket/certificate.tf
+++ aws/example/cloudfront_custom_certificate_logging_s3bucket/certificate.tf
@@ -1,7 +1,7 @@
 module "certificate_using_module" {
   //source = "git::https://github.com/dfds-frontend/terraform-modules.git//aws/acm-certificate-type-split/import-certificate" // To be used with tagging as well, this line is to serve as example for how this should be referenced in real world scenarios. The example here always refers to current local copy, hence the source definition below.
   region = "${local.region}"
-  source = "./../../acm-certificate-type-split/import-certificate/"
+  source = "./../../acm-certificate-import-certificate/"
   private_key = "${file("${path.module}/certificate-private-key.pem")}" //var.private_key maybe use: file("${path.module}/hello.txt")
   certificate_body = "${file("${path.module}/certificate-body.pem")}"
   certificate_chain = "${file("${path.module}/certificate-chain.pem")}"
