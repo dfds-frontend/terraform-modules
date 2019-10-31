b5f3264ffafb35a8dad57cf48d0047c275c21898
Dieb Sami Maher (DFDS A/S) <samdi@DFDS.COM>
2019-10-31 10:02:48 +0100

    added missing lambda association to cf s3 cf

--- aws/cloudfront/cloudfront_single_origin_s3/main.tf
+++ aws/cloudfront/cloudfront_single_origin_s3/main.tf
@@ -8,6 +8,8 @@ module "aws_cf_dist_s3" {
         is_s3_origin = true
         domain_name = var.s3_bucket_domain_name
         origin_id = "${local.s3bucket_origin_id}"
+        lambda_function_association_lambda_arn = "${var.request_lambda_edge_function_arn}"
+        lambda_function_association_include_body = "${var.request_lambda_edge_function_include_body}"
     }]
     default_cache_behavior = {
         allowed_methods = ["GET", "HEAD"]
