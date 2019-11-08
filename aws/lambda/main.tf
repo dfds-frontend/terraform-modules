529c46ddbac0ed87b621b71071248be6f0cfb1d7
Dieb Sami Maher (DFDS A/S) <samdi@DFDS.COM>
2019-11-08 22:23:24 +0100

    main file

--- aws/lambda/main.tf
+++ aws/lambda/main.tf
@@ -1,3 +1,24 @@
+terraform {
+  required_version = "~> 0.12.2"
+}
+
+resource "aws_lambda_function" "lambda" {
+  function_name = "${var.lambda_function_name}"
+  role          = "${aws_iam_role.role.arn}"
+  handler       = "${var.lambda_function_handler}.handler"
+  runtime       = "${var.runtime}"
+  filename      = "${var.filename}"
+  publish = "${var.publish}"
+  
+  dynamic "environment" {
+    for_each = length(keys(var.lambda_env_variables)) > 0 ? [1]: []
+
+    content {
+      variables = "${var.lambda_env_variables}"
+    }
+  }
+}
+
 resource "aws_iam_role" "role" {
   name = "${var.lambda_role_name}"
  
