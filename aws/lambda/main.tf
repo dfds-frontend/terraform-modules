b87cdb9435d29cea6e6047184a751538e496649c
Dieb Sami Maher (DFDS A/S) <samdi@DFDS.COM>
2019-12-04 15:28:53 +0100

    lambda_edge update

--- aws/lambda/main.tf
+++ aws/lambda/main.tf
@@ -8,17 +8,16 @@ resource "aws_lambda_function" "lambda" {
   role          = "${aws_iam_role.role.arn}"
   handler       = "${var.lambda_function_handler}.handler"
   runtime       = "${var.runtime}"
-  # filename      = "${var.filename}"
   filename = "lambda.zip"
   publish = "${var.publish}"
   
-  dynamic "environment" {
-    for_each = length(keys(var.lambda_env_variables)) > 0 ? [1]: []
+  # dynamic "environment" { # not allowed on lambda edge
+  #   for_each = length(keys(var.lambda_env_variables)) > 0 ? [1]: []
 
-    content {
-      variables = "${var.lambda_env_variables}"
-    }
-  }
+  #   content {
+  #     variables = "${var.lambda_env_variables}"
+  #   }
+  # }
 }
 
 resource "aws_iam_role" "role" {
@@ -68,38 +67,8 @@ resource "aws_iam_role_policy" "cloudwatch_logs" {
 EOF
 }
 
-
-
 data "archive_file" "lambda_zip" {
     type        = "zip"
     source_file  = "${var.filename}"
     output_path = "lambda.zip"
 }
-
-# resource "aws_lambda_function" "my_lambda" {
-#   filename = "lambda.zip"
-#   source_code_hash = "${data.archive_file.lambda_zip.output_base64sha256}"
-#   function_name = "my_lambda"
-#   role = "${aws_iam_role.lambda.arn}"
-#   description = "Some AWS lambda"
-#   handler = "index.handler"
-#   runtime = "nodejs4.3"
-
-
-  
-#   function_name = "${var.lambda_function_name}"
-#   source_code_hash = "${data.archive_file.lambda_zip.output_base64sha256}"
-#   role          = "${aws_iam_role.role.arn}"
-#   handler       = "${var.lambda_function_handler}.handler"
-#   runtime       = "${var.runtime}"
-#   filename      = "${var.filename}"
-#   publish = "${var.publish}"
-  
-#   dynamic "environment" {
-#     for_each = length(keys(var.lambda_env_variables)) > 0 ? [1]: []
-
-#     content {
-#       variables = "${var.lambda_env_variables}"
-#     }
-#   }  
-# }
