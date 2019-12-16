d890c8916943c5f28803ce12319b322ea57dd42d
Sami Maher Dieb <samdi@DFDS.COM>
2019-12-16 10:28:24 +0100

    fixes

--- aws/acm-certificate/main.tf
+++ aws/acm-certificate/main.tf
@@ -42,7 +42,7 @@ resource "aws_acm_certificate" "cert" {
 resource "aws_route53_record" "validation" {
   count = local.create_certificate && var.validation_method == "DNS" && local.validate_certificate ? length(local.distinct_domain_names) : 0
 
-  zone_id = var.zone_id
+  zone_id = var.dns_zone_id
   name    = element(local.validation_domains, count.index)["resource_record_name"]
   type    = element(local.validation_domains, count.index)["resource_record_type"]
   ttl     = 60
