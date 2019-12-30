a15c85eea9e89a53a77e993be106e659223019b3
Sami Maher Dieb <samdi@DFDS.COM>
2019-12-30 16:51:58 +0100

    reverted on ACM outputs

--- aws/acm-certificate/outputs.tf
+++ aws/acm-certificate/outputs.tf
@@ -7,28 +7,28 @@ output "certificate_arn" {
   value       = element(concat(aws_acm_certificate.cert.*.arn, [""]), 0)
 }
 
-# output "acm_certificate_domain_validation_options" {
-#   description = "A list of attributes to feed into other resources to complete certificate validation. Can have more than one element, e.g. if SANs are defined. Only set if DNS-validation was used."
-#   value       = flatten(aws_acm_certificate.cert.*.domain_validation_options)
-# }
+output "acm_certificate_domain_validation_options" {
+  description = "A list of attributes to feed into other resources to complete certificate validation. Can have more than one element, e.g. if SANs are defined. Only set if DNS-validation was used."
+  value       = flatten(aws_acm_certificate.cert.*.domain_validation_options)
+}
 
-# output "acm_certificate_validation_emails" {
-#   description = "A list of addresses that received a validation E-Mail. Only set if EMAIL-validation was used."
-#   value       = flatten(aws_acm_certificate.cert.*.validation_emails)
-# }
+output "acm_certificate_validation_emails" {
+  description = "A list of addresses that received a validation E-Mail. Only set if EMAIL-validation was used."
+  value       = flatten(aws_acm_certificate.cert.*.validation_emails)
+}
 
 output "validation_route53_record_fqdns" {
   description = "List of FQDNs built using the zone domain and name."
   value       = aws_route53_record.validation.*.fqdn
 }
 
-# output "distinct_domain_names" {
-#   description = "List of distinct domains names used for the validation."
-#   value       = local.distinct_domain_names
-# }
+output "distinct_domain_names" {
+  description = "List of distinct domains names used for the validation."
+  value       = local.distinct_domain_names
+}
 
-# output "validation_domains" {
-#   description = "List of distinct domain validation options. This is useful if subject alternative names contain wildcards."
-#   value       = local.validation_domains
-# }
+output "validation_domains" {
+  description = "List of distinct domain validation options. This is useful if subject alternative names contain wildcards."
+  value       = local.validation_domains
+}
 
