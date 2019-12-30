8a1b570e3f005cf7a775c34c70d580e59e9a4601
Sami Maher Dieb <samdi@DFDS.COM>
2019-12-30 11:18:43 +0100

    ACM attempt to fix missing validation

--- aws/acm-certificate/main.tf
+++ aws/acm-certificate/main.tf
@@ -40,21 +40,33 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
+# resource "aws_route53_record" "validation" {
+#   count = var.create_certificate && var.validation_method == "DNS" && var.validate_certificate ? length(local.distinct_domain_names) : 0
+
+#   zone_id = var.dns_zone_id
+#   name    = element(local.validation_domains, count.index)["resource_record_name"]
+#   type    = element(local.validation_domains, count.index)["resource_record_type"]
+#   ttl     = 60
+
+#   records = [
+#     element(local.validation_domains, count.index)["resource_record_value"]
+#   ]
+
+#   allow_overwrite = var.validation_allow_overwrite_records
+
+#   depends_on = [aws_acm_certificate.cert]
+# }
+
 resource "aws_route53_record" "validation" {
+  # count = "${var.deploy ? length(local.domain_names) : 0}"
   count = var.create_certificate && var.validation_method == "DNS" && var.validate_certificate ? length(local.distinct_domain_names) : 0
-
-  zone_id = var.dns_zone_id
-  name    = element(local.validation_domains, count.index)["resource_record_name"]
-  type    = element(local.validation_domains, count.index)["resource_record_type"]
-  ttl     = 60
-
-  records = [
-    element(local.validation_domains, count.index)["resource_record_value"]
-  ]
+  name = "${lookup(aws_acm_certificate.main[0].domain_validation_options[count.index], "resource_record_name")}"
+  type = "${lookup(aws_acm_certificate.main[0].domain_validation_options[count.index], "resource_record_type")}"
+  zone_id ="${var.dns_zone_id}" #" "${var.zone_id}"
+  records = ["${lookup(aws_acm_certificate.main[0].domain_validation_options[count.index], "resource_record_value")}"]
 
   allow_overwrite = var.validation_allow_overwrite_records
-
-  depends_on = [aws_acm_certificate.cert]
+  ttl = 60
 }
 
 
@@ -67,6 +79,9 @@ resource "aws_acm_certificate_validation" "cert" {
   certificate_arn = aws_acm_certificate.cert[0].arn
 
   validation_record_fqdns = aws_route53_record.validation.*.fqdn
+
+    certificate_arn = "${aws_acm_certificate.main[0].arn}"
+  validation_record_fqdns = "${aws_route53_record.validation.*.fqdn}"
 }
 
 # data "template_file" "breakup_san" {
