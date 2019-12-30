55387ae6a58c2185f9404189ba5f85aacac3546e
Sami Maher Dieb <samdi@DFDS.COM>
2019-12-30 16:49:54 +0100

    reverted on ACM

--- aws/acm-certificate/main.tf
+++ aws/acm-certificate/main.tf
@@ -3,88 +3,58 @@ terraform {
 }
 
   
-# locals {
-#   # create_certificate = true
-#   # validate_certificate = true
-
-  
-#   // Get distinct list of domains and SANs
-#   distinct_domain_names = distinct(concat([var.domain_name], [for s in var.subject_alternative_names : replace(s, "*.", "")]))
-
-#   // Copy domain_validation_options for the distinct domain names
-#   validation_domains = var.create_certificate ? [for k, v in aws_acm_certificate.cert[0].domain_validation_options : tomap(v) if contains(local.distinct_domain_names, v.domain_name)] : []
-
-  
-# }
-
-# # locals {
-# #   // Get distinct list of domains and SANs
-# #   distinct_domain_names = distinct(concat([var.domain_name], data.template_file.breakup_san.*.rendered))
-
-# #   // Copy domain_validation_options for the distinct domain names
-# #   validation_domains = [for k, v in aws_acm_certificate.cert.domain_validation_options : tomap(v) if contains(local.distinct_domain_names, v.domain_name)]
-# # }
-
-# resource "aws_acm_certificate" "cert" {
-#   count = var.create_certificate ? 1 : 0
-
-#   domain_name               = var.domain_name
-#   subject_alternative_names = var.subject_alternative_names
-#   validation_method         = var.validation_method
-
-#   # tags = var.tags
-
-#   lifecycle {
-#     create_before_destroy = true
-#     ignore_changes = ["subject_alternative_names"] # workaround to https://github.com/terraform-providers/terraform-provider-aws/issues/8531
-#   }
-# }
-
-# resource "aws_route53_record" "validation" {
-#   count = var.create_certificate && var.validation_method == "DNS" && var.validate_certificate ? length(local.distinct_domain_names) : 0
-
-#   zone_id = var.dns_zone_id
-#   name    = element(local.validation_domains, count.index)["resource_record_name"]
-#   type    = element(local.validation_domains, count.index)["resource_record_type"]
-#   ttl     = 60
-
-#   records = [
-#     element(local.validation_domains, count.index)["resource_record_value"]
-#   ]
-
-#   allow_overwrite = var.validation_allow_overwrite_records
-
-#   depends_on = [aws_acm_certificate.cert]
-# }
-
 locals {
-  # domain_names = ["samdi2.dfds.cloud", "www.samdi2.dfds.cloud"]#  "${concat([var.domain_name], var.subject_alternative_names)}"
-  domain_names = "${concat([var.domain_name], var.subject_alternative_names)}"
+  # create_certificate = true
+  # validate_certificate = true
+
+  
+  // Get distinct list of domains and SANs
+  distinct_domain_names = distinct(concat([var.domain_name], [for s in var.subject_alternative_names : replace(s, "*.", "")]))
+
+  // Copy domain_validation_options for the distinct domain names
+  validation_domains = var.create_certificate ? [for k, v in aws_acm_certificate.cert[0].domain_validation_options : tomap(v) if contains(local.distinct_domain_names, v.domain_name)] : []
+
+  
 }
 
+# locals {
+#   // Get distinct list of domains and SANs
+#   distinct_domain_names = distinct(concat([var.domain_name], data.template_file.breakup_san.*.rendered))
+
+#   // Copy domain_validation_options for the distinct domain names
+#   validation_domains = [for k, v in aws_acm_certificate.cert.domain_validation_options : tomap(v) if contains(local.distinct_domain_names, v.domain_name)]
+# }
 
 resource "aws_acm_certificate" "cert" {
-  count = "${var.create_certificate ? 1 :0}"
-  domain_name = "${local.domain_names[0]}"
-  subject_alternative_names = "${slice(local.domain_names, 1, length(local.domain_names))}"
-  validation_method = "DNS"
+  count = var.create_certificate ? 1 : 0
+
+  domain_name               = var.domain_name
+  subject_alternative_names = var.subject_alternative_names
+  validation_method         = var.validation_method
+
+  # tags = var.tags
+
   lifecycle {
     create_before_destroy = true
     ignore_changes = ["subject_alternative_names"] # workaround to https://github.com/terraform-providers/terraform-provider-aws/issues/8531
   }
 }
 
-
 resource "aws_route53_record" "validation" {
-  # count = "${var.deploy ? length(local.domain_names) : 0}"
-  count = var.create_certificate && var.validation_method == "DNS" && var.validate_certificate ? length(local.domain_names) : 0
-  name = "${lookup(aws_acm_certificate.cert[0].domain_validation_options[count.index], "resource_record_name")}"
-  type = "${lookup(aws_acm_certificate.cert[0].domain_validation_options[count.index], "resource_record_type")}"
-  zone_id ="${var.dns_zone_id}" #" "${var.zone_id}"
-  records = ["${lookup(aws_acm_certificate.cert[0].domain_validation_options[count.index], "resource_record_value")}"]
+  count = var.create_certificate && var.validation_method == "DNS" && var.validate_certificate ? length(local.distinct_domain_names) : 0
+
+  zone_id = var.dns_zone_id
+  name    = element(local.validation_domains, count.index)["resource_record_name"]
+  type    = element(local.validation_domains, count.index)["resource_record_type"]
+  ttl     = 60
+
+  records = [
+    element(local.validation_domains, count.index)["resource_record_value"]
+  ]
 
   allow_overwrite = var.validation_allow_overwrite_records
-  ttl = 60
+
+  depends_on = [aws_acm_certificate.cert]
 }
 
 
@@ -94,14 +64,10 @@ resource "aws_route53_record" "validation" {
 resource "aws_acm_certificate_validation" "cert" {
   count = var.create_certificate && var.validation_method == "DNS" && var.validate_certificate && var.wait_for_validation ? 1 : 0
 
-  # certificate_arn = aws_acm_certificate.cert[0].arn
+  certificate_arn = aws_acm_certificate.cert[0].arn
 
-  # validation_record_fqdns = aws_route53_record.validation.*.fqdn
-
-  certificate_arn = "${aws_acm_certificate.cert[0].arn}"
-  validation_record_fqdns = aws_route53_record.validation.*.fqdn #"${aws_route53_record.validation.0.fqdn}"
+  validation_record_fqdns = aws_route53_record.validation.*.fqdn
 }
-
 
 # data "template_file" "breakup_san" {
 #   count = length(var.subject_alternative_names)
