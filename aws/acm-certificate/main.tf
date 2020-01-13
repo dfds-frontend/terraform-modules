9af83d6343364a0955716118437c06938fd71673
samidbb <40062553+samidbb@users.noreply.github.com>
2020-01-13 11:41:29 +0100

    cleanup

--- aws/acm-certificate/main.tf
+++ aws/acm-certificate/main.tf
@@ -1,29 +1,14 @@
 terraform {
   required_version = "~> 0.12.2"
 }
-
   
-locals {
-  # create_certificate = true
-  # validate_certificate = true
-
-  
+locals { 
   // Get distinct list of domains and SANs
   distinct_domain_names = distinct(concat([var.domain_name], [for s in var.subject_alternative_names : replace(s, "*.", "")]))
 
   // Copy domain_validation_options for the distinct domain names
-  validation_domains = var.create_certificate ? [for k, v in aws_acm_certificate.cert[0].domain_validation_options : tomap(v) if contains(local.distinct_domain_names, v.domain_name)] : []
-
-  
+  validation_domains = var.create_certificate ? [for k, v in aws_acm_certificate.cert[0].domain_validation_options : tomap(v) if contains(local.distinct_domain_names, v.domain_name)] : []  
 }
-
-# locals {
-#   // Get distinct list of domains and SANs
-#   distinct_domain_names = distinct(concat([var.domain_name], data.template_file.breakup_san.*.rendered))
-
-#   // Copy domain_validation_options for the distinct domain names
-#   validation_domains = [for k, v in aws_acm_certificate.cert.domain_validation_options : tomap(v) if contains(local.distinct_domain_names, v.domain_name)]
-# }
 
 resource "aws_acm_certificate" "cert" {
   count = var.create_certificate ? 1 : 0
@@ -57,7 +42,6 @@ resource "aws_route53_record" "validation" {
   depends_on = [aws_acm_certificate.cert]
 }
 
-
 # Validate the certificate using the DNS validation records created
 # This resource represents a successful validation of an ACM certificate in concert with other resources.
 # WARNING: This resource implements a part of the validation workflow. It does not represent a real-world entity in AWS, therefore changing or deleting this resource on its own has no immediate effect.
@@ -68,42 +52,3 @@ resource "aws_acm_certificate_validation" "cert" {
 
   validation_record_fqdns = aws_route53_record.validation.*.fqdn
 }
-
-# data "template_file" "breakup_san" {
-#   count = length(var.subject_alternative_names)
-
-#   template = replace(var.subject_alternative_names[count.index], "*.", "")
-# }
-
-# # Create the certificate request
-# resource "aws_acm_certificate" "cert" {
-#   domain_name               = var.domain_name
-#   subject_alternative_names = var.subject_alternative_names
-#   validation_method = "DNS"
-#   lifecycle {
-#     create_before_destroy = true
-#     ignore_changes = ["subject_alternative_names"] # workaround to https://github.com/terraform-providers/terraform-provider-aws/issues/8531
-#   }
-# }
-
-# resource "aws_route53_record" "validation" {
-#   count = var.validation_method == "DNS" ? length(local.distinct_domain_names) : 0
-
-#   zone_id = var.dns_zone_id
-#   name    = local.validation_domains[count.index]["resource_record_name"]
-#   type    = local.validation_domains[count.index]["resource_record_type"]
-#   ttl     = 60
-
-#   records = [
-#     local.validation_domains[count.index]["resource_record_value"]
-#   ]
-# }
-
-# Validate the certificate using the DNS validation records created
-# This resource represents a successful validation of an ACM certificate in concert with other resources.
-# WARNING: This resource implements a part of the validation workflow. It does not represent a real-world entity in AWS, therefore changing or deleting this resource on its own has no immediate effect.
-# resource "aws_acm_certificate_validation" "cert" {
-#   count = "${var.validation_method == "DNS" && var.wait_for_validation ? 1 : 0 }"
-#   certificate_arn = "${aws_acm_certificate.cert.arn}"
-#   validation_record_fqdns = "${aws_route53_record.validation.*.fqdn}"
-# }
