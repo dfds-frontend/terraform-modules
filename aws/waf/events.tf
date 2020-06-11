b645140e006d7c91e915138a5b7a8da3a8e8489c
Sami Maher Dieb <samdi@DFDS.COM>
2020-06-11 14:05:06 +0200

    updating

--- aws/waf/events.tf
+++ aws/waf/events.tf
@@ -5,30 +5,32 @@ resource "aws_cloudwatch_event_rule" "reputation_lists_parser" {
   schedule_expression = "rate(1 hour)"
 }
 
-# resource "aws_cloudwatch_event_target" "reputation_lists_parser" {
-#   count = "${var.reputation_lists_protection_activated ? 1 : 0}"
-#   rule = aws_cloudwatch_event_rule.reputation_lists_parser[count.index].name
-#   arn  = "${element(concat(aws_lambda_function.reputation_lists_parser.*.arn, [""]), 0)}"  // "${aws_lambda_function.reputation_lists_parser.arn}"
+resource "aws_cloudwatch_event_target" "reputation_lists_parser" {
+  count = "${var.reputation_lists_protection_activated ? 1 : 0}"
+  rule = aws_cloudwatch_event_rule.reputation_lists_parser[count.index].name
+  arn  = "${element(concat(aws_lambda_function.reputation_lists_parser.*.arn, [""]), 0)}"  // "${aws_lambda_function.reputation_lists_parser.arn}"
 
-#   input = <<INPUT
-# {
-#   "lists": [
-#     {
-#       "url": "https://www.spamhaus.org/drop/drop.txt"
-#     },
-#     {
-#       "url": "https://check.torproject.org/exit-addresses",
-#       "prefix": "ExitAddress"
-#     },
-#     {
-#       "url": "https://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt"
-#     }
-#   ],
-#   "apiType": "waf",
-#   "region": "${var.aws_region}",
-#   "ipSetIds": [
-#     "${element(concat(aws_waf_ipset.waf_reputation_set.*.id, [""]), 0)}" 
-#   ]
-# }
-# INPUT
-# }
+  input = <<INPUT
+{
+  "lists": [
+    {
+      "url": "https://www.spamhaus.org/drop/drop.txt"
+    },
+    {
+      "url": "https://check.torproject.org/exit-addresses",
+      "prefix": "ExitAddress"
+    },
+    {
+      "url": "https://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt"
+    }
+  ],
+  "apiType": "waf",
+  "region": "${var.aws_region}",
+  "ipSetIds": [
+    "${aws_waf_ipset.waf_reputation_set.id}" 
+  ]
+}
+INPUT
+}
+
+# "${element(concat(aws_waf_ipset.waf_reputation_set.*.id, [""]), 0)}" 
