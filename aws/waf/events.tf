2a56b55e4b5b786d3669d37dea1d5a07bb35c843
Sami Maher Dieb <samdi@DFDS.COM>
2020-06-11 14:07:31 +0200

    updating

--- aws/waf/events.tf
+++ aws/waf/events.tf
@@ -6,9 +6,9 @@ resource "aws_cloudwatch_event_rule" "reputation_lists_parser" {
 }
 
 resource "aws_cloudwatch_event_target" "reputation_lists_parser" {
-  count = "${var.reputation_lists_protection_activated ? 1 : 0}"
-  rule = aws_cloudwatch_event_rule.reputation_lists_parser[count.index].name
-  arn  = "${element(concat(aws_lambda_function.reputation_lists_parser.*.arn, [""]), 0)}"  // "${aws_lambda_function.reputation_lists_parser.arn}"
+  # count = "${var.reputation_lists_protection_activated ? 1 : 0}"
+  rule = aws_cloudwatch_event_rule.reputation_lists_parser.name # aws_cloudwatch_event_rule.reputation_lists_parser[count.index].name
+  arn  = "${aws_lambda_function.reputation_lists_parser.arn}" // "${element(concat(aws_lambda_function.reputation_lists_parser.*.arn, [""]), 0)}" 
 
   input = <<INPUT
 {
