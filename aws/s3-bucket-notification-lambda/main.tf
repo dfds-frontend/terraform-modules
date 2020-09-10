eae5665936f0f3441f09597434f2a657e4d5884a
Sami Maher Dieb <samdi@DFDS.COM>
2020-09-10 13:22:52 +0200

    s3-bucket-notification-lambda

---
+++ aws/s3-bucket-notification-lambda/main.tf
@@ -0,0 +1,20 @@
+resource "aws_lambda_permission" "allow_bucket_access" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::${var.bucket_id}" 
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = var.bucket_id
+
+  lambda_function {
+    lambda_function_arn = var.lambda_arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = var.src_prefix  
+    # filter_suffix       = ".gz"
+  }
+
+  depends_on = [aws_lambda_permission.allow_bucket_access]
+}
