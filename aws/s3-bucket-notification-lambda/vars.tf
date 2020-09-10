5815c3e9c4661443e7708e08ce01dbfd2372d63d
samidbb <40062553+samidbb@users.noreply.github.com>
2020-09-10 13:31:15 +0200

    s3-bucket-notification-lambda (#45)

---
+++ aws/s3-bucket-notification-lambda/vars.tf
@@ -0,0 +1,13 @@
+variable "lambda_arn" {
+  type = string
+}
+
+variable "bucket_id" {
+  type = string
+}
+
+variable src_prefix {
+  type        = string
+  default     = ""
+  description = "Prefix for files which will trigger the lambda function. Use to only process a single directory within the source bucket"
+}
