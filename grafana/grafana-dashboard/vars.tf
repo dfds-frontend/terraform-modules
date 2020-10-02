4618153c985efc92aae9e23140ad2c00b68f7be2
Sami Maher Dieb <samdi@DFDS.COM>
2020-10-02 23:51:38 +0200

    minor fix

--- grafana/grafana-dashboard/vars.tf
+++ grafana/grafana-dashboard/vars.tf
@@ -9,7 +9,7 @@ variable "input_file" {
 
 
 variable "dashboard_values" {
-  description 
+  description =
   <<DESCRIPTION
    "It depends on the variables defined in the dashboard input json. Recommended value map {"dasboard_title" = "some_title", "env" = "some_environment" }
   DESCRIPTION
