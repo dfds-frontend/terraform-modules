b89aab5cd05db22d3e51eb97f2e03e194f63e4fb
Sami Maher Dieb <samdi@DFDS.COM>
2020-10-02 23:56:11 +0200

    test

--- grafana/grafana-dashboard/vars.tf
+++ grafana/grafana-dashboard/vars.tf
@@ -9,8 +9,8 @@ variable "input_file" {
 
 
 variable "dashboard_values" {
-  description = <<TEXT  
-   It depends on the variables defined in the dashboard input json. Recommended value map {"dasboard_title" = "some_title", "env" = "some_environment"}
-  TEXT
+  # description = <<TEXT  
+  #  It depends on the variables defined in the dashboard input json. Recommended value map {"dasboard_title" = "some_title", "env" = "some_environment"}
+  # TEXT
   type        = map(string)
 } 
