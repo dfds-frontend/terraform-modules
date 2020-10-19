e9abe46de2666cd4ee2f7372fd8102be495d6900
Sami Maher Dieb <samdi@DFDS.COM>
2020-10-19 13:01:46 +0200

    run lambda permission

---
+++ aws/run-lambda-permission/main.tf
@@ -0,0 +1,6 @@
+resource "aws_lambda_permission" "this" {
+  action        = "lambda:InvokeFunction"
+  function_name = var.function_name
+  principal     = var.principal
+  source_arn    = var.source_arn
+}
---
+++ aws/run-lambda-permission/vars.tf
@@ -0,0 +1,11 @@
+variable function_name {
+    type = string
+}
+
+variable principal {
+    type = string
+}
+
+variable source_arn {
+    type = string
+}
