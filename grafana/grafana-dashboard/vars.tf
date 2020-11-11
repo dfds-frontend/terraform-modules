b3924ea8e4e36a9adf8dc02f012763583a33835f
Sami Maher Dieb <samdi@DFDS.COM>
2020-11-11 16:15:34 +0100

    removed unused code

--- grafana/grafana-dashboard/vars.tf
+++ grafana/grafana-dashboard/vars.tf
@@ -1,8 +1,3 @@
-# variable "title" {
-#     description = "Dashboard title. Should unique within a folder"
-#     type = string
-# }
-
 variable "folder_id" {
   default = null
 }
@@ -11,7 +6,3 @@ variable "input_file" {
   description = "File containing dashboard in json format"
   type = string
 }
-
-# variable "additional_override_values" {
-#     type = map(string)
-# }
