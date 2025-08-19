3f2a696b73c02faa43b9f917fce0b8266919c12a
Audun Nes <aunes@dfds.com>
2025-08-19 11:23:38 +0200

    refactor: improve variable formatting and consistency in vars.tf

--- aws/cloudfront/cloudfront_multiple_origins/vars.tf
+++ aws/cloudfront/cloudfront_multiple_origins/vars.tf
@@ -4,7 +4,7 @@ variable "origins" {
 
 variable "cache_behaviors" {
   description = "List of chache behaviors assigned that cloudfront should support."
-  default = []
+  default     = []
 }
 
 variable "default_cache_behavior" {
@@ -13,46 +13,46 @@ variable "default_cache_behavior" {
 
 variable "web_acl_id" {
   description = "If you're using AWS WAF to filter CloudFront requests, the Id of the AWS WAF web ACL that is associated with the distribution."
-  type = string
-  default = null
+  type        = string
+  default     = null
 }
 
 variable "comment" {
   description = "A short description of the cloudfront distribution. Comments used to enable the user to distinquish between cloudfront distributions."
-  default = ""
+  default     = ""
 }
 
 variable "acm_certificate_arn" {
-  description = "The arn of the certificate that covers the custom domain when aliases are added to the cloudfront distribution."  
-  type = string
-  default = null
+  description = "The arn of the certificate that covers the custom domain when aliases are added to the cloudfront distribution."
+  type        = string
+  default     = null
 }
 
 variable "custom_ssl_security_policy" {
   description = "Choose the security policy that you want CloudFront to use for HTTPS connections. A security policy determines two settings: the SSL/TLS protocol that CloudFront uses to communicate with viewers, and the cipher that CloudFront uses to encrypt the content that it returns to viewers. When you use a custom SSL certificate and SNI, you must use TLSv1 or later. We recommend that you specify TLSv1.1_2016 unless your users are using browsers or devices that do not support TLSv1.1 or later. When you use a custom SSL certificate and dedicated IP addresses, we recommend that you use TLSv1. In this configuration, the TLSv1_2016, TLSv1.1_2016 and TLSv1.2_2018 security policies are not available"
-  default = "TLSv1.2_2018"
+  default     = "TLSv1.2_2018"
 }
 
 
 variable "aliases" {
-  default = []
-  type = "list"
+  default     = []
+  type        = list(any)
   description = "The list of custom domains to be used to reach the cloudfront distribution instead of the auto-generated cloudfront domain (xxxx.cloudfront.net)."
 }
 
 variable "origin_access_identity" {
-  default = ""
+  default     = ""
   description = "The path that identifies the origin access identity to be used for accessing s3 bucket origins."
 }
 
 variable "logging_enable" {
   description = "Enable/Disable Cloudfront access logs."
-  default = false
+  default     = false
 }
 
 variable "logging_include_cookies" {
   description = "Specifies whether you want CloudFront to include cookies in access logs (default: false)"
-  default = false
+  default     = false
 }
 
 
@@ -62,29 +62,29 @@ variable "logging_bucket" {
 
 variable "logging_prefix" {
   description = "Folder path inside s3 bucket where Cloudfront access logs will be stored."
-}  
-
-variable "price_class" {
-    description = "Price class for Cloudfront."
-    default = "PriceClass_100"
 }
 
-variable "wait_for_deployment" {  
-  default = true
+variable "price_class" {
+  description = "Price class for Cloudfront."
+  default     = "PriceClass_100"
+}
+
+variable "wait_for_deployment" {
+  default     = true
   description = "If enabled, the resource will wait for the distribution status to change from InProgress to Deployed. Setting this to false will skip the process."
 }
 
 
 variable "custom_error_responses" {
   description = "Custom error response to be used in dynamic block. Documentation is here: https://www.terraform.io/docs/providers/aws/r/cloudfront_distribution.html#custom-error-response-arguments"
-  type = any
-  default = []
+  type        = any
+  default     = []
 }
 
 variable "tags" {
   description = "A mapping of tags to assign to the resource"
   type        = map(string)
-  default     = {
+  default = {
     "Managed by" : "Terraform"
-    }
+  }
 }
