1660d195c748173d48024e68906784d8e1ba8d2a
Audun Nes <aunes@dfds.com>
2025-08-19 09:44:51 +0200

    refactor: clean up formatting and improve command consistency in verify-terraform-plan.sh

--- boilerplate/pipeline_scripts/azure-devops/safemode/verify-terraform-plan.sh
+++ boilerplate/pipeline_scripts/azure-devops/safemode/verify-terraform-plan.sh
@@ -2,7 +2,7 @@
 working_dir=$1
 tfplan_output_file=$2
 
-############################################################## 
+##############################################################
 # Setting this env variable will activate Terraform debug log
 # export TF_LOG=DEBUG
 ##############################################################
@@ -13,21 +13,21 @@ tfplan_output_file=${tfplan_output_file,,}
 
 cd $working_dir
 
-terragrunt plan --terragrunt-source-update --terragrunt-non-interactive -input=false -out=$tfplan_output_file
+terragrunt plan --source-update --non-interactive -input=false -out=$tfplan_output_file
 
 if [ ! -f "$tfplan_output_file" ]; then
-    echo "##vso[task.logissue type=error]Plan file not found. Exiting..."
-    exit 1    
+	echo "##vso[task.logissue type=error]Plan file not found. Exiting..."
+	exit 1
 fi
 
 echo "Creating json representation of Terragrunt Plan..."
 tfplan_output_file_json="$tfplan_output_file".json
 
-terragrunt show -json $tfplan_output_file > $tfplan_output_file_json | tr -d '\r\n'
+terragrunt show -json $tfplan_output_file >$tfplan_output_file_json | tr -d '\r\n'
 echo "Done creating json representation of Terragrunt Plan."
 
 resources_for_deletion_list=$(cat $tfplan_output_file_json | jq '.resource_changes[] | select(.change.actions[] | contains("delete"))')
-if [ -n "$resources_for_deletion_list" ]; then          
-    echo "##vso[task.logissue type=error]Attempt to delete resources through pipeline. Please delete the resources manually"
-    exit 1
+if [ -n "$resources_for_deletion_list" ]; then
+	echo "##vso[task.logissue type=error]Attempt to delete resources through pipeline. Please delete the resources manually"
+	exit 1
 fi
