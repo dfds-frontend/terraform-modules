88e5f97eb8957a8dc00696d8ebebc2ed66eca2d0
Audun Nes <aunes@dfds.com>
2025-08-19 17:02:42 +0200

    refactor: update output value references to use splat expressions for consistency across Terraform files

--- aws/acm-certificate/outputs.tf
+++ aws/acm-certificate/outputs.tf
@@ -1,16 +1,16 @@
 output "certificate_arn" {
   description = "The ARN of the certificate"
-  value       = element(concat(aws_acm_certificate.cert[0].arn, [""]), 0)
+  value       = element(concat(aws_acm_certificate.cert.*.arn, [""]), 0)
 }
 
 output "acm_certificate_domain_validation_options" {
   description = "A list of attributes to feed into other resources to complete certificate validation. Can have more than one element, e.g. if SANs are defined. Only set if DNS-validation was used."
-  value       = flatten(aws_acm_certificate.cert[0].domain_validation_options)
+  value       = flatten(aws_acm_certificate.cert.*.domain_validation_options)
 }
 
 output "acm_certificate_validation_emails" {
   description = "A list of addresses that received a validation E-Mail. Only set if EMAIL-validation was used."
-  value       = flatten(aws_acm_certificate.cert[0].validation_emails)
+  value       = flatten(aws_acm_certificate.cert.*.validation_emails)
 }
 
 output "validation_route53_record_fqdns" {
@@ -27,4 +27,3 @@ output "validation_domains" {
   description = "List of distinct domain validation options. This is useful if subject alternative names contain wildcards."
   value       = local.validation_domains
 }
-
--- aws/s3-bucket/outputs.tf
+++ aws/s3-bucket/outputs.tf
@@ -1,11 +1,11 @@
 output "bucket_domain_name" {
-  value = element(concat(aws_s3_bucket.bucket[0].bucket_regional_domain_name, list("")), 0)
+  value = element(concat(aws_s3_bucket.bucket.*.bucket_regional_domain_name, list("")), 0)
 }
 
 output "bucket_name" {
-  value = element(concat(aws_s3_bucket.bucket[0].id, list("")), 0)
+  value = element(concat(aws_s3_bucket.bucket.*.id, list("")), 0)
 }
 
 output "bucket_arn" {
-  value = element(concat(aws_s3_bucket.bucket[0].arn, list("")), 0)
+  value = element(concat(aws_s3_bucket.bucket.*.arn, list("")), 0)
 }
