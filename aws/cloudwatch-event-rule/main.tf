5a6b7367c4b8a35bd0af4c7e31f7b37c29ed7d5f
Audun Nes <aunes@dfds.com>
2025-08-19 11:34:55 +0200

    refactor: clean up formatting and improve alignment in main.tf

--- aws/cloudwatch-event-rule/main.tf
+++ aws/cloudwatch-event-rule/main.tf
@@ -1,19 +1,14 @@
-terraform {
-  backend "s3" {}
-}
-
-
 resource "aws_cloudwatch_event_rule" "this" {
   name                = "${var.name_prefix}_event_rule"
-  is_enabled  = true
+  is_enabled          = true
   description         = "Run lambda to monitor hosts"
   schedule_expression = var.event_schedule
 }
 
 resource "aws_cloudwatch_event_target" "this" {
-  rule = aws_cloudwatch_event_rule.this.name
+  rule      = aws_cloudwatch_event_rule.this.name
   target_id = var.cloudwatch_event_target_id # if missing then it will generate random
-  arn  = var.cloudwatch_event_target_arn
+  arn       = var.cloudwatch_event_target_arn
 
   input = var.event_input
 }
