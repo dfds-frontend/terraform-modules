d92349f38e2b0b83df8d9e0d0c17fe0eb092ca8f
Audun Nes <aunes@dfds.com>
2025-08-20 14:24:56 +0200

    refactor: standardize formatting of resource attributes in CloudWatch log group

--- aws/cloudwatch-log-group/main.tf
+++ aws/cloudwatch-log-group/main.tf
@@ -1,8 +1,7 @@
+# trunk-ignore-all(checkov/CKV_AWS_158)
 resource "aws_cloudwatch_log_group" "log_group" {
-  count = var.deploy ? 1 : 0
-  name  = var.name
-
-  tags = var.tags
-
+  count             = var.deploy ? 1 : 0
+  name              = var.name
+  tags              = var.tags
   retention_in_days = var.retention_days
 }
