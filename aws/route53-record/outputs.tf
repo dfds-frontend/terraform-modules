6cc5b2cc54979a22f3ded41061d87dc2fbde9f87
Audun Nes <aunes@dfds.com>
2025-08-20 08:42:12 +0200

    refactor: update output value assignments to use splat expressions for consistency in Route53 records

--- aws/route53-record/outputs.tf
+++ aws/route53-record/outputs.tf
@@ -1,7 +1,7 @@
 output "alias_record_name" {
-  value = aws_route53_record.alias_record[0].name
+  value = aws_route53_record.alias_record.*.name
 }
 
 output "cname_record_name" {
-  value = aws_route53_record.cname_record[0].name
+  value = aws_route53_record.cname_record.*.name
 }
