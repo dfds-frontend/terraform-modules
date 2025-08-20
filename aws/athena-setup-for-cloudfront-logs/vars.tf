096e48892ddfeaba6accb252bf8953bb7fbcb280
Audun Nes <aunes@dfds.com>
2025-08-20 14:11:38 +0200

    refactor: standardize output and variable formatting across multiple files

--- aws/athena-setup-for-cloudfront-logs/vars.tf
+++ aws/athena-setup-for-cloudfront-logs/vars.tf
@@ -1,25 +1,25 @@
-variable "athena_workgroup_name" { 
-    type = string
+variable "athena_workgroup_name" {
+  type = string
 }
 
-variable "athena_output_location" { 
-    type = string
+variable "athena_output_location" {
+  type = string
 }
 
-variable "bucket_query_results" { 
-    type = string
+variable "bucket_query_results" {
+  type = string
 }
 
-variable "source_bucket_path" { 
-    type = string
+variable "source_bucket_path" {
+  type = string
 }
 
 variable "athena_db_name" {
-    type = string
-    description = "Athena Database name"
+  type        = string
+  description = "Athena Database name"
 }
 
 variable "athena_table_name" {
-    type = string
-    description = "Default table name"
+  type        = string
+  description = "Default table name"
 }
